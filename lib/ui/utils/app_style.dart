5aa65e04f181b72a6af117c83f45fe2aac9b8f46
anas mohamed <anas2003610@gmail.com>
2024-08-27 02:30:46 +0300

    add some util to poject

---
+++ lib/ui/utils/app_style.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:todoproject/ui/utils/app_color.dart';
+
+abstract class AppStyle {
+  static const TextStyle appBarStyle = TextStyle(
+      fontSize: 30, fontWeight: FontWeight.bold, color: AppColors.white);
+  static const TextStyle titlesTextStyle = TextStyle(
+      fontSize: 27, fontWeight: FontWeight.bold, color: AppColors.primary);
+
+  static const TextStyle bottomSheetTitle = TextStyle(
+      fontSize: 18, fontWeight: FontWeight.w700, color: AppColors.black);
+
+  static const TextStyle bodyTextStyle = TextStyle(
+      fontSize: 12, fontWeight: FontWeight.w600, color: AppColors.black);
+  static const TextStyle unSelectedCalendarDayStyle = TextStyle(
+      color: AppColors.black, fontWeight: FontWeight.bold, fontSize: 15);
+
+  static TextStyle selectedCalendarDayStyle =
+      unSelectedCalendarDayStyle.copyWith(color: AppColors.primary);
+  static const TextStyle appBarDarkStyle = TextStyle(
+      fontSize: 30, fontWeight: FontWeight.bold, color: AppColors.black);
+}
---
+++ lib/ui/utils/app_theme.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:todoproject/ui/utils/app_color.dart';
+
+abstract class AppTheme {
+  static ThemeData light = ThemeData(
+    useMaterial3: false,
+    scaffoldBackgroundColor: AppColors.bgColor,
+    canvasColor: AppColors.white,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primary,
+      primary: AppColors.primary,
+      secondary: AppColors.white,
+      onPrimary: AppColors.white,
+    ),
+    appBarTheme: const AppBarTheme(
+        backgroundColor: AppColors.primary, centerTitle: true),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: AppColors.white,
+      selectedItemColor: AppColors.primary,
+      unselectedItemColor: Colors.grey,
+    ),
+  );
+
+  static ThemeData dark = ThemeData(
+    useMaterial3: false,
+    scaffoldBackgroundColor: AppColors.bgDark,
+    canvasColor: AppColors.black,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primary,
+      primary: AppColors.primary,
+      secondary: AppColors.black,
+      onPrimary: AppColors.black,
+    ),
+    appBarTheme: const AppBarTheme(
+        backgroundColor: AppColors.primary, centerTitle: true),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: AppColors.black,
+      selectedItemColor: AppColors.primary,
+      unselectedItemColor: Colors.white,
+    ),
+  );
+}
