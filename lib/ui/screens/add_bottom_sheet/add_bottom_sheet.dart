ee260ebbeb14ec99eb43650499ac5c89e4f462fc
anas mohamed <anas2003610@gmail.com>
2024-08-29 19:42:41 +0300

    first design for bottom sheet

--- lib/ui/screens/add_bottom_sheet/add_bottom_sheet.dart
+++ lib/ui/screens/add_bottom_sheet/add_bottom_sheet.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 class AddBottomSheet extends StatelessWidget {
@@ -5,7 +7,24 @@ class AddBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        const Text('add new task'),
+        const TextField(
+          decoration: InputDecoration(hintText: 'enter task title '),
+        ),
+        const TextField(
+          decoration: InputDecoration(hintText: 'enter task description'),
+        ),
+        const Text('select date'),
+        const Text('28/8/2024'),
+        const Spacer(),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Add'),
+        ),
+      ],
+    );
   }
 
   static void ShowBottomSheet(BuildContext context) {
